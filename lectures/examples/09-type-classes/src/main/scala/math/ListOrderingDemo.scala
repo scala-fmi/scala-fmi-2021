3795f814434fe4a8d1633660df30a3a5c413ba61
nnyx7 <nnyx77@gmail.com>
2021-05-16 13:23:00 +0300

    Fix listOrdering properly

--- lectures/examples/09-type-classes/src/main/scala/math/ListOrderingDemo.scala
+++ lectures/examples/09-type-classes/src/main/scala/math/ListOrderingDemo.scala
@@ -1,27 +1,28 @@
 package math
 
+import scala.annotation.tailrec
+
 object ListOrderingDemo extends App {
-  implicit def listOrdering[A: Ordering]: Ordering[List[A]] = new Ordering[List[A]] {
-    def compare(x: List[A], y: List[A]): Int = {
-      val aOrdering = Ordering[A]
-      import aOrdering.mkOrderingOps
+  implicit def listOrdering[A: Ordering]: Ordering[List[A]] = (x: List[A], y: List[A]) => {
+    val aOrdering = Ordering[A]
+    import aOrdering.mkOrderingOps
 
-      @tailrec
-      def loop(x: List[A], y: List[A]): Int = {
-        if (x.isEmpty && y.isEmpty) 0
-        else if (x.isEmpty) -1
-        else if (y.isEmpty) 1
-        else if (x.head < y.head) -1
-        else if (y.head < x.head) 1
-        else loop(x.tail, y.tail)
-      }
-
-      loop(x, y)
+    @tailrec
+    def loop(x: List[A], y: List[A]): Int = {
+      if (x.isEmpty && y.isEmpty) 0
+      else if (x.isEmpty) -1
+      else if (y.isEmpty) 1
+      else if (x.head < y.head) -1
+      else if (y.head < x.head) 1
+      else loop(x.tail, y.tail)
     }
+
+    loop(x, y)
   }
 
   val sortedList = List(
     List(1, 2, 3),
+    List(1, 3, 2),
     List(3, 4),
     List.empty[Int],
     List(1, 1)
